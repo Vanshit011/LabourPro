b6fb3dc0394d90168de67302872fd56c08c8d597
Vanshit Patel <vanshitpatel10@gmail.com>
2025-07-26 19:32:03 +0530

    vite changes

--- labourpro_frontend/vite.config.js
+++ labourpro_frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // <-- Important for deployment
+  server: {
+    historyApiFallback: true, // <-- 👈 SPA routing fallback
+  }
 })
